8a861366ce6fa0ecdbb6615804b068b09db635a0
talawww <esaymatira@gmail.com>
2023-09-23 15:29:43 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/main', 'StudentController::student');
+$routes->post('/create', 'StudentController::create');
+$routes->get('/delete/(:any)', 'StudentController::delete/$1');
+$routes->get('/edit/(:any)', 'StudentController::edit/$1');
